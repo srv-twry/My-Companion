eab83ea99456f2fc949199870eb419ab7dd51c33
udhay <udhayakumar241998@gmail.com>
2018-10-18 16:12:52 +0530

    added async task fr saving flash card

--- app/src/main/java/com/example/srv_twry/studentcompanion/AddFlashCardActivity.java
+++ app/src/main/java/com/example/srv_twry/studentcompanion/AddFlashCardActivity.java
@@ -3,6 +3,7 @@ package com.example.srv_twry.studentcompanion;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -73,28 +74,36 @@ public class AddFlashCardActivity extends AppCompatActivity {
      */
 
     private void onClickAddTask() {
-        //We don't want empty topics in the database hence this check
-        String inputQuestion = questionEditText.getText().toString();
-        String inputAnswer = answerEditText.getText().toString();
-        if (inputQuestion.length() == 0 || inputAnswer.length() == 0) {
-            Toast.makeText(getBaseContext(), R.string.both_the_fields_are_mandatory,Toast.LENGTH_SHORT).show();
-            return;
-        }
 
-        ContentValues contentValues = new ContentValues();
+        new AsyncTask(){
+            @Override
+            protected Object doInBackground(Object[] objects) {
 
-        contentValues.put(DatabaseContract.FlashCardsEntry.FLASH_CARD_TOPIC_NAME, topicName);
-        contentValues.put(DatabaseContract.FlashCardsEntry.FLASH_CARD_QUESTION, inputQuestion);
-        contentValues.put(DatabaseContract.FlashCardsEntry.FLASH_CARD_ANSWER,inputAnswer);
+                //We don't want empty topics in the database hence this check
+                String inputQuestion = questionEditText.getText().toString();
+                String inputAnswer = answerEditText.getText().toString();
+                if (inputQuestion.length() == 0 || inputAnswer.length() == 0) {
+                    AddFlashCardActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getBaseContext(), R.string.both_the_fields_are_mandatory,Toast.LENGTH_SHORT).show();
+                        }
+                    });
 
-        Uri uri = getContentResolver().insert(DatabaseContract.FlashCardsEntry.CONTENT_URI_FLASH_CARDS, contentValues);
+                    return null;
+                }
 
+                ContentValues contentValues = new ContentValues();
 
-        if(uri != null) {
-            Toast.makeText(getBaseContext(), R.string.created_the_flash_card_successfully,Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(getBaseContext(), R.string.unable_to_create_the_card,Toast.LENGTH_SHORT).show();
-        }
+                contentValues.put(DatabaseContract.FlashCardsEntry.FLASH_CARD_TOPIC_NAME, topicName);
+                contentValues.put(DatabaseContract.FlashCardsEntry.FLASH_CARD_QUESTION, inputQuestion);
+                contentValues.put(DatabaseContract.FlashCardsEntry.FLASH_CARD_ANSWER,inputAnswer);
+
+                getContentResolver().insert(DatabaseContract.FlashCardsEntry.CONTENT_URI_FLASH_CARDS, contentValues);
+
+                return null;
+            }
+        }.execute();
 
         //Returning to the FlashCardsHomeActivity
         finish();
@@ -118,6 +127,9 @@ public class AddFlashCardActivity extends AppCompatActivity {
         String answer = cursor.getString(cursor.getColumnIndex(DatabaseContract.FlashCardsEntry.FLASH_CARD_ANSWER));
         questionEditText.setText(question);
         answerEditText.setText(answer);
+
+        button.setText("Update");
+
     }
 
     private void prepareSaveLayout(){
